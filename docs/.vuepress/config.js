bcbecb90e93506d4f665d654d6141d774265b43a
Daniel Cukier <danicuki@gmail.com>
2023-08-09 16:07:24 -0300

    added favicon to docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -94,7 +94,7 @@ export default defineUserConfig({
   ],
 
   head: [
-    // ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["link", { rel: "icon", href: "/favicon.png" }],
     [
       "meta",
       {
